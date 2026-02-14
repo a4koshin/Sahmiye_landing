e7ebf0a10059d057e710fad894cbae0096f7d792
a4koshin <mankajr11@gmail.com>
2026-02-14 10:52:06 +0300

    Terms data is added

---
+++ lib/terms.ts
@@ -0,0 +1,63 @@
+interface TermsData{
+    id:number;
+    title:string;
+    description:string
+}
+
+export const terms : TermsData [] = [
+    {
+        id:0,
+        title:" Eligibility and Use",
+        description:"You must be at least 18 years old, or have permission from a parent or legal guardian, to use this Platform. You agree to use the Platform only for lawful purposes and in compliance with applicable laws."
+    },
+    {
+        id:1,
+        title:"Account Responsibility",
+        description:"You are responsible for maintaining the confidentiality of your account credentials and for all activities conducted under your account. You must notify us immediately of any unauthorized access or security breach."
+    },
+    {
+        id:2,
+        title:"Listings and Content",
+        description:"All property listings, images, descriptions, and related materials are provided by users, agents, or third parties. We do not guarantee the accuracy, completeness, or reliability of any listing and are not responsible for losses or damages arising from reliance on such information."
+    },
+    {
+        id:7,
+        title:"Payments",
+        description:"Some features or services may require payment. By making a payment, you agree to provide accurate billing information and authorize us or our partners to process charges. Payments are non-refundable unless required by law."
+    },
+    {
+        id:3,
+        title:"Intellectual Property",
+        description:"All platform content, including logos, text, designs, and software, is owned by the Company or its licensors. You may not copy, modify, distribute, or reuse any content without prior written permission."
+    },
+    {
+        id:4,
+        title:"Disclaimer and Limitation of Liability",
+        description:"The Platform is provided on an “as is” and “as available” basis without warranties of any kind. We are not liable for any indirect, incidental, or consequential damages resulting from your use of the Platform."
+    },
+    {
+        id:5,
+        title:" Suspension and Termination",
+        description:"We reserve the right to suspend or terminate access to the Platform at any time if these Terms or applicable laws are violated."
+    },
+    {
+        id:6,
+        title:"Changes to Terms",
+        description:"These Terms may be updated periodically. Continued use of the Platform after changes are made constitutes acceptance of the updated Terms."
+    },
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
